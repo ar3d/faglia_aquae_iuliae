cb6ffccc850067feca8431f3e6df2744d10f7119
ar3d <info@ar3d.it>
2021-09-18 12:47:45 +0200

    ok

--- terremoto_giugno_847/layers/847_epicentro_molise_lazio_merid_2.js
+++ terremoto_giugno_847/layers/847_epicentro_molise_lazio_merid_2.js
@@ -3,6 +3,6 @@ var json_847_epicentro_molise_lazio_merid_2 = {
 "name": "847_epicentro_molise_lazio_merid_2",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
 "features": [
-{ "type": "Feature", "properties": { "Toponimo": "<b>Sannio</b>", "Localizzaz": "<br>Latitudine: <b>41.623</b><br>Longitudine: <b>14.159</b><br>Topologia di epicentro: <b>Calcolato</b>", "Descrizion": "Data: <b>giugno 847</b><br>Intensità epicentrale sulla scala Mercalli (MCS): <b>8</b><br>Intensità massima sulla scala Mercalli (MCS): <b>9</b><br>Numero di osservazioni macrosismiche: <b>6</b><br>Magnetudo equivalente: <b>5.6</b>" }, "geometry": { "type": "Point", "coordinates": [ 14.159, 41.623 ] } }
+{ "type": "Feature", "properties": { "Toponimo": "<b>Sannio</b>", "Localizzaz": "<br>Latitudine: <b>41.623</b><br>Longitudine: <b>14.159</b><br>Tipologia di epicentro: <b>Calcolato</b>", "Descrizion": "Data: <b>giugno 847</b><br>Intensità epicentrale sulla scala Mercalli (MCS): <b>8</b><br>Intensità massima sulla scala Mercalli (MCS): <b>9</b><br>Numero di osservazioni macrosismiche: <b>6</b><br>Magnetudo equivalente: <b>5.6</b>" }, "geometry": { "type": "Point", "coordinates": [ 14.159, 41.623 ] } }
 ]
 }
